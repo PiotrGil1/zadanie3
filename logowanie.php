7f0287798301cc4691cbdfba5d2c4d7395295de0
PiotrGil1 <130742010+PiotrGil1@users.noreply.github.com>
2023-04-18 12:58:51 +0200

    Update logowanie.php

--- logowanie.php
+++ logowanie.php
@@ -7,7 +7,7 @@ $mysqli = new mysqli('localhost','root', '', 'sprawdzanie');
 
 if($mysqli->connect_errno){
     echo 'nie udało się połączyć do MySQL: ' . $mysqli->connect_error;
-    exit;
+    exit();
 }
 else{
     $msg='';
